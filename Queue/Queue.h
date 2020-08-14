4098b5c05e5c20db58759180234362a77ef5f462
youkeyao <66420911+youkeyao@users.noreply.github.com>
2020-08-14 18:07:45 +0800

    Update Queue.h

--- Queue/Queue.h
+++ Queue/Queue.h
@@ -1,6 +1,8 @@
 #ifndef Queue_List_h
 #define Queue_List_h
-    #include "List\List.cpp" //以列表为基类，派生出栈模板类
+
+    #include "List\List.hpp" //以列表为基类，派生出栈模板类
+
     template <typename T>
     class Queue: public List<T> { //将列表的首/末端作为栈顶/底
     public: //size()、empty()以及其它开放接口，均可直接沿用
@@ -8,4 +10,5 @@
         T dequeue() { return this->remove ( this->first() ); } //出队：首部删除
         T& front() { return this->first()->data; } //队首
     };
+
 #endif
