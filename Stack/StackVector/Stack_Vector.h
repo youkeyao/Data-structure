46d2ac728c47bc2cbf22794a373a36f03fc35757
youkeyao <66420911+youkeyao@users.noreply.github.com>
2020-08-14 18:28:45 +0800

    Update Stack_Vector.h

--- Stack/StackVector/Stack_Vector.h
+++ Stack/StackVector/Stack_Vector.h
@@ -1,6 +1,8 @@
 #ifndef Stack_Vector_h
 #define Stack_Vector_h
+
     #include "Vector\Vector.hpp" //以向量为基类，派生出栈模板类
+
     template <typename T>
     class Stack: public Vector<T> { //将向量的首/末端作为栈底/顶
     public: //size()、empty()以及其它开放接口，均可直接沿用
@@ -8,4 +10,5 @@
         T pop() { return this->remove ( this->size() - 1 ); } //出栈：等效于删除向量的末元素
         T& top() { return ( *this ) [this->size() - 1]; } //取顶：直接返回向量的末元素
     };
+
 #endif
